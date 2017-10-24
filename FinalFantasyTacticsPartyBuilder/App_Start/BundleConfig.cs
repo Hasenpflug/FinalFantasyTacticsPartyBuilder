682b9bbf4263c0a8f331550867bb75917c4cf561
hasenb <hasenb@BHASEN-D1-05>
2017-10-24 17:14:18 -0400

    - Added custom font to look like font used in game - Added ripped images from game for UI - Started creating base CSS classes

--- FinalFantasyTacticsPartyBuilder/App_Start/BundleConfig.cs
+++ FinalFantasyTacticsPartyBuilder/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace FinalFantasyTacticsPartyBuilder
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/Fonts/stylesheet.css"));
         }
     }
 }
--- FinalFantasyTacticsPartyBuilder/Migrations/Configuration.cs
+++ FinalFantasyTacticsPartyBuilder/Migrations/Configuration.cs
@@ -6,7 +6,7 @@ namespace FinalFantasyTacticsPartyBuilder.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<FinalFantasyTacticsPartyBuilder.Models.FFTContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<FFTContext>
     {
         public Configuration()
         {
